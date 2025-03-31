e7442f7f0ba9981a8dbb1dd45bdb43f39b68d130
lhachete <mhmonllor@hotmail.com>
2025-03-31 16:21:41 +0200

    APIREST GET y POST, ejemplos del Indio

---
+++ src/main/java/com/example/demo/controllers/RestEndpoints.java
@@ -0,0 +1,19 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class RestEndpoints {
+
+    @RequestMapping("/course")
+    public Course getEndpoint(@RequestParam(value="name",defaultValue = "Spring Boot", required = false) String name,
+                              @RequestParam(value="chapterCount",defaultValue = "2", required = false) int chapterCount) {
+        return new Course(name,chapterCount);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/register/course")
+    public String saveCourse(@RequestBody Course course) {
+        return "Your course named "+ course.getName() + " with" + course.getChapterCount() + " chapters has been saved";
+    }
+
+}
